d22c8d6892c2ff0838d503ac9db03d3678fd1ca1
rachit-goyal <rachitgoyal2011@gmail.com>
2024-05-02 20:23:14 +0530

    blink lib added

--- Blink/build.gradle.kts
+++ Blink/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("maven-publish")
+    apply {
+        `maven-publish`
+    }
 }
 
 android {
