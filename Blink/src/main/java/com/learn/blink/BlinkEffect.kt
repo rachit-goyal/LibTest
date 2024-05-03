ee2906a7a149b689305c9c40c8a73df04ef543d9
rachit-goyal <rachitgoyal2011@gmail.com>
2024-05-03 19:36:26 +0530

    blink lib added

--- Blink/src/main/java/com/learn/blink/BlinkEffect.kt
+++ Blink/src/main/java/com/learn/blink/BlinkEffect.kt
@@ -2,27 +2,18 @@ package com.learn.blink
 
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.content.Context
 import android.graphics.Color
 import android.view.View
 import android.view.animation.Animation
+import android.widget.Toast
 
 /**
 created by Rachit on 5/2/2024.
  */
 object BlinkEffect {
-    fun blink(view: View) {
+    fun blink(context: Context) {
         // adding the color to be shown
-        val animator: ObjectAnimator = ObjectAnimator.ofInt(
-            view, "backgroundColor", Color.YELLOW,
-            Color.RED, Color.GREEN
-        )
-        // duration of one color
-        animator.duration = 500;
-        animator.setEvaluator(ArgbEvaluator())
-        // color will be shown in reverse manner
-        animator.repeatCount = Animation.REVERSE
-        // Repeat up to infinite time
-        animator.repeatCount = Animation.INFINITE
-        animator.start()
+        Toast.makeText(context, "Blink", Toast.LENGTH_SHORT).show()
     }
 }
